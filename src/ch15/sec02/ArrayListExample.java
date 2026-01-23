2db26ad5984b916dc58a2d161857ed9fa02888ef
younggeun <qwazsx1346@gmail.com>
2026-01-23 11:32:48 +0900

    ArrayListStringMain2 작업 중

---
+++ src/ch15/sec02/ArrayListExample.java
@@ -0,0 +1,18 @@
+package ch15.sec02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    /* 배열은 크기가 고정. 크기 변경이 안 된다.
+    크기 변경해서 사용하고 싶으면 새로운 배열을 만들어서 복사도 하고
+    값도 넣고 써야했다.
+    */
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("하하");
+        list.add("후후");
+        list.add("크크");
+        System.out.println(list);
+    }
+}
