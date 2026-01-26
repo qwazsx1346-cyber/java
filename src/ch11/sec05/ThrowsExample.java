e0199a565c8cb1def1eb19dfeb5a830393d6cd16
younggeun <qwazsx1346@gmail.com>
2026-01-26 11:54:22 +0900

    예외처리 마무리

---
+++ src/ch11/sec05/ThrowsExample.java
@@ -0,0 +1,35 @@
+package ch11.sec05;
+
+public class ThrowsExample {
+    public static void main(String[] args) {
+        try {
+            findClass();
+        } catch (ClassNotFoundException e) {
+            System.out.println("클래스를 찾을 수 없다!!");
+        } catch (Exception e) {
+            System.out.println("예외 발생!");
+        }
+        findClass2();
+    }
+
+    /* 메소드가 예외를 던질 때 해결방법 2가지 */
+
+
+    //1. 나도 던진다. throws ClassNotFoundException
+    public static void findClass() throws ClassNotFoundException {
+        Class.forName("java.lang.String2"); //대문자 . 호출은 static메소드. forName메소드는 예외를 던진다.
+    }
+
+    //2. 해결한다. (try-catch)
+    public static void findClass2() {
+      try{
+        Class.forName("java.lang.String2"); //대문자 . 호출은 static메소드. forName메소드는 예외를 던진다.
+      } catch (Exception e) {
+        System.out.println("클래스를 찾을 수 없습니다.");
+      }
+
+
+  }
+
+
+}
